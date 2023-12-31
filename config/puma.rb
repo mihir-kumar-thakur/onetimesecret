e0f53086c838ef9d2a1595a31640c1b64efb3235
Mihir Kumar Thakur <>
2023-12-31 08:41:14 +0530

    puma daemoinze

--- config/puma.rb
+++ config/puma.rb
@@ -12,6 +12,8 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
+daemonize
+
 # Specifies that the worker count should equal the number of processors in production.
 if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
