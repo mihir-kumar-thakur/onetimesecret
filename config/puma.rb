8f852b22fe71a55379a83647ae3644fb498f8a75
Mihir Kumar Thakur <>
2023-12-31 10:05:06 +0530

    daemonize true

--- config/puma.rb
+++ config/puma.rb
@@ -12,7 +12,7 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-daemonize
+daemonize true
 
 # Specifies that the worker count should equal the number of processors in production.
 if ENV["RAILS_ENV"] == "production"
