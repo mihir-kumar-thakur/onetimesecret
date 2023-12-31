5d37764ccca53fd7c35846eb1b6c4fc4424b8d33
Mihir Kumar Thakur <>
2023-12-31 23:22:28 +0530

    change db

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem "rails", "~> 7.1.2"
 gem "sprockets-rails"
 
 # Use mysql as the database for Active Record
-gem "mysql2", "~> 0.5"
+gem "pg", "~> 1.1"
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_30_032222) do
-  create_table "secrets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "secrets", force: :cascade do |t|
     t.text "message"
     t.boolean "burned"
     t.datetime "burned_at"
