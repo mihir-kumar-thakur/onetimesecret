39a6040379639fc06769acba524b57ad9957e0fa
Mihir Kumar Thakur <>
2024-01-02 10:12:16 +0530

    minimum secure token length 24

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -1,6 +1,6 @@
 class Secret < ApplicationRecord
   validates :message, presence: true, unless: :burned
-  has_secure_token :token, length: 8
+  has_secure_token :token, length: 24
 
   def burn!
     self.burned = true
